99d1c6fb144bc2d9303f3ffa4f234ad1457b3205
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2023-02-13 23:11:32 +0200

    fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom'
 import { getTrendingMovie } from 'services/movie-api';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { HomeWrapper, HomeTitle, MovieList, ListItem } from './Home.styled';
 
-
 const Home = () => {
-    const [movies, setMovies] = useState([]);
-    const location = useLocation();
+  const [movies, setMovies] = useState([]);
+  const location = useLocation();
 
-    useEffect(() => {
-        getTrendingMovie().then(setMovies);
-    }, []);
+  useEffect(() => {
+    getTrendingMovie().then(setMovies);
+  }, []);
 
-    return (
-        <HomeWrapper>
-            <HomeTitle>Trending today</HomeTitle>
-            <MovieList>
-                {movies.map(({ id, title }) => {
-                    return (
-                        <li>
-                            <ListItem state={{ from: location }} to={`/movies/${id}`}/>
-                            {title}
-                        </li>
-                    );
-                })}
-            </MovieList>
-        </HomeWrapper>
-    );
+  return (
+    <HomeWrapper>
+      <HomeTitle>Trending today</HomeTitle>
+      <MovieList>
+        {movies.map(({ id, title }) => {
+          return (
+            <li key={id}>
+              <ListItem state={{ from: location }} to={`/movies/${id}`}>
+                {title}
+              </ListItem>
+            </li>
+          );
+        })}
+      </MovieList>
+    </HomeWrapper>
+  );
 };
 
 export default Home;
